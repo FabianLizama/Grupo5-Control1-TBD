9b3db435dc29f4329e0ddd63410ef91690e12192
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-04-07 12:40:09 -0400

    Fix 8.sql

--- 8.sql
+++ 8.sql
@@ -3,6 +3,6 @@ JOIN product AS prod ON comp.company_id = prod.company_id
 JOIN detailed_selling_product AS dsp ON prod.product_id = dsp.product_id
 JOIN detailed_selling AS ds ON dsp.detailed_selling_id = ds.detailed_selling_id
 JOIN orders AS ord ON ds.detailed_selling_id = ord.detailed_selling_id
-WHERE ord.order_date >= CURDATE() - INTERVAL 1 YEAR
+WHERE ord.order_date >= CURRENT_DATE - INTERVAL '1 YEAR'
 GROUP BY comp.company_name
 ORDER BY TotalRevenue DESC;
