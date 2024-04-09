ceab060797afb2af68586f011f4f41aeb15418b6
Fabián Lizama <fabian.lizama@usach.cl>
2024-04-08 20:49:31 -0400

    runStatements fix

--- runStatements.sql
+++ runStatements.sql
@@ -85,8 +85,8 @@ JOIN Detailed_Selling ds ON c.customer_id = ds.customer_id
 JOIN Orders o ON ds.detailed_selling_id = o.detailed_selling_id
 JOIN Detailed_Selling_Product dsp ON ds.detailed_selling_id = dsp.detailed_selling_id
 JOIN Product p ON dsp.product_id = p.product_id
-WHERE MONTH(o.order_date) = MONTH(CURRENT_DATE())
-AND YEAR(o.order_date) = YEAR(CURRENT_DATE())
+WHERE EXTRACT(MONTH FROM o.order_date) = EXTRACT(MONTH FROM CURRENT_DATE)
+AND EXTRACT(YEAR FROM o.order_date) = EXTRACT(YEAR FROM CURRENT_DATE)
 GROUP BY c.customer_name, p.product_name
 ORDER BY total_amount_money DESC;
 
@@ -101,7 +101,7 @@ JOIN
 JOIN
     Detailed_Selling_Product dsp ON ds.detailed_selling_id = dsp.detailed_selling_id
 WHERE
-    o.order_date >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+    o.order_date >= (CURRENT_DATE - INTERVAL '1 MONTH')
 GROUP BY
     DATE(o.order_date)
 ORDER BY
